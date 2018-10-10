eec230c73fc7a322d161a9cdcb202dc99eb14c0d
Your Name <you@example.com>
2018-10-10 21:42:40 +0300

    machine learning now work nbc

--- MoviesReview/Controllers/HomeController.cs
+++ MoviesReview/Controllers/HomeController.cs
@@ -36,27 +36,56 @@ namespace MoviesReview.Controllers
         {
             int userID = ((User)Session["User"]).Id;
 
+            var jamal = db.Reviews.Where(review => review.UserID == userID).ToList();
+
             int[] selectedGeneres = db.Reviews.Where(review => review.UserID == userID).Select(review => review.Movie.GenereID).ToArray();
             int[][] dataset = new int[selectedGeneres.Length][];
-                
+
+            Dictionary<int, int> mapper = new Dictionary<int, int>();
+            Dictionary<int, int> mapperOpsite = new Dictionary<int, int>();
+
+
+            int counter = 0;
+
             for (int genereIndex = 0; genereIndex < selectedGeneres.Length; genereIndex++)
             {
                 dataset[genereIndex] = new int[1];
+
+                if (!mapper.ContainsKey(selectedGeneres[genereIndex]))
+                {
+                    mapper[selectedGeneres[genereIndex]] = counter;
+                    mapperOpsite[counter] = selectedGeneres[genereIndex];
+
+                    counter++;
+                }
+
+                
+            }
+
+            int[] mappedLabels = new int[selectedGeneres.Length];
+
+            for (int i = 0; i < selectedGeneres.Length; i++)
+            {
+                mappedLabels[i] = mapper[selectedGeneres[i]];
             }
 
             //Array.Clear(dataset, 0, dataset.Length);
 
+
             var learner = new NaiveBayesLearning();
 
-            NaiveBayes nb = learner.Learn(dataset, selectedGeneres);
+            NaiveBayes nb = learner.Learn(dataset, mappedLabels);
 
-            int selectedGenere = nb.Decide(new int[default(int)]);
+            int[] prediction = new int[] { default(int)};
 
+            int selectedGenereMapped = nb.Decide(prediction);
+
+            int selectedIndex = mapperOpsite[selectedGenereMapped];
 
             // Select random movie from the user favorite genere
             Random rnd = new Random();
 
-            List<Movie> allMoviesInGenere = db.Generes.First(x => x.Id == 1).Movies;
+            List<Movie> allMoviesInGenere = db.Generes.First(x => x.Id == selectedIndex).Movies;
 
             Movie selectedMovie = allMoviesInGenere[rnd.Next(allMoviesInGenere.Count)];
 
